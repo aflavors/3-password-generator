5630c21bc5503d803d050de164bbdda520647a31
Akeem Flavors <akeemflavors@gmail.com>
2020-05-06 23:08:51 -0400

    Created welcomePrompt function and addEventListener

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//-----Define Variables
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//-----Functions
+// Welcome user and confirm they want to generate a password
+function welcomePrompt() {
+  confirm("Please answer the following questions to generate your password. Click ok to begin.");
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +16,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+//-----Call Functions
 // Add event listener to generate button
+generateBtn.addEventListener("click", welcomePrompt);
 generateBtn.addEventListener("click", writePassword);
