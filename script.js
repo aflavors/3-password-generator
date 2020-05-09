d2fba66ccca010048da62a64c141d54be53a92c1
Akeem Flavors <akeemflavors@gmail.com>
2020-05-09 18:54:26 -0400

    Created if statemtn for number password criteria

--- script.js
+++ script.js
@@ -57,7 +57,25 @@ function writePassword() {
         alert("Your new password is " + makeUpperCasePasswd())
       }
   }
-  
+  else if (welcomePrompt === "n") {
+    var lengthPrompt = prompt( "How many characters long would you like your password to be? Please enter a number between 8 and 128");
+    var lengthNumber = parseInt(lengthPrompt);
+    console.log(lengthNumber);
+
+    function makeNumberPasswd() {
+      var passwd = '';
+      var chars = '0123456789';
+      for (i=1; i<lengthNumber; i++) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        passwd += chars.charAt(c)
+      }
+      return passwd;
+    }
+    var confirmPassword = confirm("Click ok for your new password")
+      if (confirmPassword === true){
+        alert("Your new password is " + makeNumberPasswd())
+      }
+  }
 
   // Don't touch below
   var password = generatePassword();
