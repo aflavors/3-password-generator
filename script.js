6356af744f5f7275d469d4753e6261140457ab77
Akeem Flavors <akeemflavors@gmail.com>
2020-05-09 18:57:03 -0400

    Created if statemetn for special character criteria

--- script.js
+++ script.js
@@ -76,6 +76,25 @@ function writePassword() {
         alert("Your new password is " + makeNumberPasswd())
       }
   }
+  else if (welcomePrompt === "s") {
+    var lengthPrompt = prompt( "How many characters long would you like your password to be? Please enter a number between 8 and 128");
+    var lengthNumber = parseInt(lengthPrompt);
+    console.log(lengthNumber);
+
+    function makeSpecialCharPasswd() {
+      var passwd = '';
+      var chars = '!@#$%^&*()_-+=`~,.<>?/|';
+      for (i=1; i<lengthNumber; i++) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        passwd += chars.charAt(c)
+      }
+      return passwd;
+    }
+    var confirmPassword = confirm("Click ok for your new password")
+      if (confirmPassword === true){
+        alert("Your new password is " + makeSpecialCharPasswd())
+      }
+  }
 
   // Don't touch below
   var password = generatePassword();
