b30a40727845933988453cb7a6d4bd48c6ede782
Akeem Flavors <akeemflavors@gmail.com>
2020-05-09 18:50:48 -0400

    Created if statement for uppercase character criteria

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var charsAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_-
 function writePassword() {
   var welcomePrompt = prompt("What kind of characters would you like to include in your password? Enter 'l' for lowercase, 'u' for uppercase, 'n' for numbers, 's' for special characters, or 'all' for all character types.")
   welcomePrompt = welcomePrompt.toLocaleLowerCase();
-
+// If statements for each letter selection
   if (welcomePrompt === "l") {
     var lengthPrompt = prompt( "How many characters long would you like your password to be? Please enter a number between 8 and 128");
     var lengthNumber = parseInt(lengthPrompt);
@@ -38,6 +38,28 @@ function writePassword() {
         alert("Your new password is " + makeLowerCasePasswd())
       }
   }
+  else if (welcomePrompt === "u") {
+    var lengthPrompt = prompt( "How many characters long would you like your password to be? Please enter a number between 8 and 128");
+    var lengthNumber = parseInt(lengthPrompt);
+    console.log(lengthNumber);
+
+    function makeUpperCasePasswd() {
+      var passwd = '';
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      for (i=1; i<lengthNumber; i++) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        passwd += chars.charAt(c)
+      }
+      return passwd;
+    }
+    var confirmPassword = confirm("Click ok for your new password")
+      if (confirmPassword === true){
+        alert("Your new password is " + makeUpperCasePasswd())
+      }
+  }
+  
+
+  // Don't touch below
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
